9fa2621d40f4574ae0d906acb4be7e505628f26b
NaOH123321 <duanhaixiao@hotmail.com>
2019-10-21 00:04:55 +0800

    commit -m token

--- application/api/controller/v1/Token.php
+++ application/api/controller/v1/Token.php
@@ -3,6 +3,8 @@ namespace app\api\controller\v1;
 
 use app\api\validate\TokenGet;
 use app\api\service\UserToken;
+use app\api\service\Token as TokenService;
+use app\lib\exception\ParameterException;
 
 class Token
 {
@@ -19,6 +21,19 @@ class Token
         $token = $wx->get();
         return [
             'token' => $token
+        ];
+    }
+
+    public function verifyToken($token='')
+    {
+        if(!$token){
+            throw new ParameterException([
+                'msg' => 'token不允许为空'
+            ]);
+        }
+        $valid = TokenService::verifyToken($token);
+        return [
+            'isValid' => $valid
         ];
     }
 }
