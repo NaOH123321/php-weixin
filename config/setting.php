45baed60fb1358ae166c309a6a9d36fa97d899a0
NaOH123321 <32951650+NaOH123321@users.noreply.github.com>
2019-10-21 16:44:59 +0800

    ...

--- config/setting.php
+++ config/setting.php
@@ -1,6 +1,7 @@
 <?php
 
 return[
-    'img_prefix'      => 'http://localhost/zerg/public/images',
+    // 'img_prefix'      => 'http://localhost/zerg/public/images',
+    'img_prefix'      => 'http://192.168.8.83/zerg/public/images',
     'token_expire_in' => 7200
 ];
